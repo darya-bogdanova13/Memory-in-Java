c9a0557a7e9c20f51caad3d865905cd1a9e4f842
darya <darya.kolycheva98@yandex.ru>
2024-04-16 04:59:13 +0300

    Задание 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 5");
+        int value = 33;
+        System.out.println("value = " + value);
+        changeValue(value);
+        System.out.println("value = " + value);
+    }
+    private static void changeValue(int value1) {
+        value1 = 22;
+        System.out.println("value1 = " + value1 );
     }
 }
