d502373bda760dcf59611e1153178f76e83d1a0e
darya <darya.kolycheva98@yandex.ru>
2024-04-16 05:39:34 +0300

    Задание 8

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,15 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Задание 7");
-        Integer [] value = new Integer[] {3, 4};
+        System.out.println("Задание 8");
+        Integer[] value = new Integer[]{3, 4};
         changeValue(value);
         System.out.println(Arrays.toString(value));
     }
-    private static void changeValue(Integer[] value) {
-        value = new Integer[] {1, 2};
-        System.out.println(Arrays.toString(value));
+
+    private static void changeValue(Integer[] value4) {
+        value4 [0] = 99;
+        System.out.println(Arrays.toString(value4));
     }
 }
+
